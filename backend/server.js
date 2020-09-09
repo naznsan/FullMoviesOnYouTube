88335b0bb5d3dd641344387a11e5f67d5fb52ff0
Justin Kim <naznsan@gmail.com>
2020-09-09 18:20:44 +1200

    Change backend port to 3001

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const uri = process.env.ATLAS_URI;
 
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
